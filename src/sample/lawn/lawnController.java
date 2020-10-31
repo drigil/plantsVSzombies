212b4c4494b2289d9545d1ecc81d1bc7ece4bb13
drigil <49494567+drigil@users.noreply.github.com>
2020-11-01 03:06:57 +0530

    Update lawnController.java

--- src/sample/lawn/lawnController.java
+++ src/sample/lawn/lawnController.java
@@ -504,7 +504,7 @@ public class lawnController implements Initializable
                 if(tile_click_count==1 && anchorPane.getChildren().isEmpty())
                 {
                     System.out.println("clicked pane inside if");
-                    System.out.println(temp_Image.getUrl());
+                    //System.out.println(temp_Image.getUrl());
 
 //                    System.out.println(anchorPane.getChildren().isEmpty() == true);
 
